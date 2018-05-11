961bd29286c57a9ec00ec08abab5de1f11686cb0
haiyangpan <598441578@qq.com>
2018-05-11 18:23:42 +0800

    制作todolist

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div id="app">
-  </div>
+ <div>
+   <input type="text" v-model="inputValue" @keyup.enter="handleEnterValue">
+   <ul>
+     <li v-for="(item, index) of list" :key="index">{{item}}</li>
+   </ul>
+ </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleEnterValue (e) {
+      this.list.push(e.target.value)
+    }
+  }
 }
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,13 @@
 <template>
-  <div></div>
+  <div>hello</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'HelloWorld'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
