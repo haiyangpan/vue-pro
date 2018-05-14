f39696a9057a8117a0ab237ff7cfeb861d5d6b85
haiyangpan <598441578@qq.com>
2018-05-14 11:09:40 +0800

    修改header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
  <div>
-   <input type="text" v-model="inputValue" @keyup.enter="handleEnterValue">
-   <ul>
-     <li v-for="(item, index) of list" :key="index">{{item}}</li>
-   </ul>
+ 	<router-view></router-view>
  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data () {
-    return {
-      inputValue: '',
-      list: []
-    }
-  },
-  methods: {
-    handleEnterValue (e) {
-      this.list.push(e.target.value)
-    }
-  }
+  name: 'App'
 }
 </script>
 
---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+ <div class="header">
+ 	index
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style>
+	.header{
+		background: red;
+	}
+</style>
